591b9bbe709f74795a64616b869bbbdf95a60fcd
gerardasr <gerardas.rem@gmail.com>
2026-02-23 19:25:56 +0200

    about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="text-block">
         <h1>gerardas remeikis</h1>
         <p className="description">
-          I am a multidisciplinary artist based in Vilnius, Lithuania. In my work I tend to focus on reexamination of structures and systems relevant to my life, analysing them in a critical, but multifaceted way. I often use irony, metaphor, word play to express my conclusions, which are often paradoxical.
+          I am an artist in Vilnius, Lithuania. In my work I tend to focus on reexamination of structures and systems relevant to my daily experience, analysing them from a critical, but empathetic point of view. I often use irony, metaphor, word play to express my conclusions, which are often inconclusive and paradoxical.
         </p>
       </div>
     </main>
